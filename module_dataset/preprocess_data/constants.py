49846e51334e8f48839aef5a0058b2d709e6b656
dungdl28d11 <phongnt98@gmail.com>
2020-03-06 15:40:09 +0700

    add features and label names

--- module_dataset/preprocess_data/constants.py
+++ module_dataset/preprocess_data/constants.py
@@ -1,2 +1,6 @@
 label_expl = {"1": "Xet xu",
               "0": "Hoa giai"}
+
+features_l = ['legal_rela', 'plaintiff_age', 'defendant_age',
+              'age_dist', 'has_children']
+LABEL_NAME = 'decision'
